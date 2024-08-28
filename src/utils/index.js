5863c54b3ab3687480261fe8968730ac18950042
aalzriqat <aalzriqat@gmail.com>
2024-08-28 15:35:25 +0300

    server link

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const serverUrl = "http://localhost:4000";
+const serverUrl = "https://scheduler-server-n9z6.onrender.com“;
 
 export const api = axios.create({
   baseURL: serverUrl,
